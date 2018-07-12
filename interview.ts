37341d2bc8f0c9c8ffb7651d335644e03e876bc4
Atul Varma <varmaa@gmail.com>
2018-07-12 17:40:57 -0400

    Documented interview.ts.

--- interview.ts
+++ interview.ts
@@ -3,16 +3,36 @@ import * as stream from 'stream';
 
 import { Question, ValidationError } from './question';
 
+/**
+ * This is a mapped type [1] consisting of properties that consist
+ * of questions whose answers map to the original property types.
+ * 
+ * [1] https://www.typescriptlang.org/docs/handbook/advanced-types.html#mapped-types
+ */
 type QuestionsFor<T> = {
   [P in keyof T]: Question<T[P]>;
 };
 
 export interface InterviewOptions<S> {
+  /** A ReadLine object for the interview. If not provided, stdio will be used. */
   rl?: readline.ReadLine;
+
+  /** The stream to which output will be written. Defaults to stdout. */
   output?: NodeJS.WriteStream;
+
+  /** A callback that will be called whenever a state change occurs. */
   onChange?: (state: S) => void;
 }
 
+/**
+ * This represents a series of questions for a user, parameterized by
+ * a type that represents the state of the interview (e.g., the answers
+ * to the questions the user has been asked).
+ * 
+ * This class has been designed to conduct interviews using multiple
+ * communication media (voice, SMS, web, etc), though at the time
+ * of this writing, only a console-based readline interface is available.
+ */
 export abstract class Interview<S> {
   rl: readline.ReadLine;
   isAsking: boolean;
@@ -29,6 +49,7 @@ export abstract class Interview<S> {
     this.onChange = options.onChange;
   }
 
+  /** Ask a question of the user. */
   ask<T>(question: Question<T>): Promise<T> {
     if (this.isAsking) {
       throw new Error('Assertion failure, we are already asking a question!');
@@ -49,6 +70,10 @@ export abstract class Interview<S> {
     });
   }
 
+  /**
+   * Ask a number of questions of the user. Some user interfaces,
+   * such as screens, may present the questions as a single form.
+   */
   async askMany<T>(questions: QuestionsFor<T>): Promise<T> {
     const result = {} as T;
 
@@ -59,8 +84,26 @@ export abstract class Interview<S> {
     return result;
   }
 
+  /**
+   * This is the core abstract method that subclasses must implement.
+   * Given a current state, it must ask any required questions and
+   * return a promise that represents the new state of the interview.
+   * 
+   * Note that the state is immutable, so the method should always
+   * create a new state object--unless the interview is over, in
+   * which case it should just return the unchanged state it was
+   * passed in.
+   * 
+   * @param state The current state of the interview.
+   */
   abstract async askNext(state: S): Promise<S>;
 
+  /**
+   * Runs the interview, asking the user questions until they
+   * are exhausted. Returns the final state of the interview.
+   * 
+   * @param initialState 
+   */
   async execute(initialState: S): Promise<S> {
     let state = initialState;
 
