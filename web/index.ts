1b02e2769aa824df1556f37f8f6dfc6d9da2c374
Atul Varma <varmaa@gmail.com>
2018-07-18 15:20:56 -0400

    Implement askMany.

--- web/index.ts
+++ web/index.ts
@@ -2,7 +2,6 @@ import { InterviewIO, QuestionsFor } from '../lib/interview-io';
 import { Question, ValidationError } from '../lib/question';
 import { Photo } from '../lib/util';
 import { TenantInterview } from '../lib/tenant-interview';
-import { resolve } from 'path';
 
 function getElement(selector: string): Element {
   const node = document.querySelector(selector);
@@ -11,6 +10,16 @@ function getElement(selector: string): Element {
   }
   return node;
 }
+
+/**
+ * This is a mapped type [1] consisting of properties that consist
+ * of question inputs whose answers map to the original property types.
+ * 
+ * [1] https://www.typescriptlang.org/docs/handbook/advanced-types.html#mapped-types
+ */
+export type QuestionInputsFor<T> = {
+  [P in keyof T]: QuestionInput<T[P]>;
+};
 
 class QuestionInput<T> {
   container: HTMLDivElement;
@@ -92,7 +101,46 @@ class WebInterviewIO extends InterviewIO {
   }
 
   async askMany<T>(questions: QuestionsFor<T>): Promise<T> {
-    throw new Error('not implemented');
+    const form = document.createElement('form');
+    const questionInputs = {} as QuestionInputsFor<T>;
+
+    for (let key in questions) {
+      const qi = new QuestionInput(questions[key]);
+      questionInputs[key] = qi;
+      form.appendChild(qi.container);
+    }
+
+    const submit = document.createElement('input');
+    submit.setAttribute('type', 'submit');
+    form.appendChild(submit);
+
+    this.root.appendChild(form);
+
+    const getResponses = async (): Promise<T|null> => {
+      const responses = {} as T;
+      let isValid = true;
+      for (let key in questionInputs) {
+        const response = await questionInputs[key].respond();
+        if (response) {
+          responses[key] = response;
+        } else {
+          isValid = false;
+        }
+      }
+      return isValid ? responses : null;
+    }
+
+    return new Promise<T>((resolve, reject) => {
+      form.onsubmit = (e) => {
+        e.preventDefault();
+        getResponses().then(responses => {
+          if (responses) {
+            this.root.removeChild(form);
+            return resolve(responses);
+          }
+        }).catch(reject);
+      };
+    });
   }
 
   notify(text: string) {
