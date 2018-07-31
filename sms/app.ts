6cd811a26410cedef0a072000693792171fc45dc
Atul Varma <varmaa@gmail.com>
2018-07-31 08:03:26 -0400

    Make SMS webhook server an optional dependency.

--- sms/app.ts
+++ sms/app.ts
@@ -1,9 +1,5 @@
-import * as http from 'http';
 import minimist from 'minimist';
-import express from 'express';
-import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import { SmsPostBody } from '../lib/sms/post-body';
 import { processMessage, SmsAppState } from './process-message';
 import { Storage, FileStorage } from '../lib/sms/storage';
 
@@ -70,20 +66,6 @@ async function getStorage(): Promise<Storage<SmsAppState>> {
   return new FileStorage(STATE_FILE);
 }
 
-function createApp(): express.Application {
-  const app = express();
-  const storagePromise = getStorage();
-
-  app.post('/sms', bodyParser.urlencoded({ extended: true }), async (req, res) => {
-    // TODO: Verify that the POST is actually coming from Twilio.
-    const twiml = await processMessage(req.body as SmsPostBody, await storagePromise);
-
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-  });
-  
-  return app;
-}
 
 async function simulate(body: string, argv: minimist.ParsedArgs): Promise<number> {
   if (!body.trim()) {
@@ -101,6 +83,18 @@ async function simulate(body: string, argv: minimist.ParsedArgs): Promise<number
   return 0;
 }
 
+async function serve(): Promise<void> {
+  const { runServer } = await import('./server');
+  const storage = await getStorage();
+
+  runServer(PORT, storage);
+}
+
+function handleError(e: Error) {
+  console.log(e.stack);
+  process.exit(1);
+}
+
 if (!module.parent) {
   const argv = minimist(process.argv.slice(2));
   const cmd = argv._[0];
@@ -108,14 +102,9 @@ if (!module.parent) {
     showHelp();
   } else if (cmd === 'simulate') {
     const body = argv._.slice(1).join(' ');
-    simulate(body, argv).then(process.exit).catch(e => {
-      console.log(e.stack);
-      process.exit(1);
-    });
+    simulate(body, argv).then(process.exit).catch(handleError);
   } else if (cmd === 'serve') {
-    http.createServer(createApp()).listen(PORT, () => {
-      console.log(`Express server listening on port ${PORT}.`);
-    });
+    serve().catch(handleError);
   } else {
     if (cmd) {
       console.log(`Invalid command "${cmd}".`);
